5d9bc722e058c1206053cffdee0fc46b232ce38d
boneill <boneill1292@gmail.com>
2016-12-06 20:38:55 -0600

    Added some services / form work for the contact page.

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
-    {       
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,6 +27,23 @@ namespace TheWorld.Controllers.Web
 
         public IActionResult About()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
+
+
             return View();
         }
 
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -7,15 +7,42 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json");
+
+            _config = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+
+            if (_env.IsDevelopment())
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                //Implement a real mail service.
+            }
+
+
             services.AddMvc();
         }
 
@@ -24,9 +51,9 @@ namespace TheWorld
         {
             if (env.IsEnvironment("Development"))
             {
-                app.UseDeveloperExceptionPage();    
+                app.UseDeveloperExceptionPage();
             }
-            
+
 
             app.UseStaticFiles();
 
