719a05960037c79ce10bb0a33234f74c791fa476
boneill <boneill1292@gmail.com>
2017-01-17 22:21:08 -0600

    Created the API

--- src/TheWorld/Models/IWorldRepository.cs
+++ src/TheWorld/Models/IWorldRepository.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace TheWorld.Models
 {
     public interface IWorldRepository
     {
         IEnumerable<Trip> GetAllTrips();
+
+        Trip GetTripByName(string tripName);
+
+        void AddTrip(Trip trip);
+
+         Task<bool> SaveChangesAsync();
+
+        void AddStop(string tripName, Stop newStop);
     }
 }
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Logging;
 using TheWorld.Services;
 using Microsoft.Extensions.Configuration;
 using TheWorld.Models;
+using Newtonsoft.Json.Serialization;
+using AutoMapper;
+using TheWorld.ViewModels;
 
 namespace TheWorld
 {
@@ -47,11 +50,14 @@ namespace TheWorld
 
             services.AddScoped<IWorldRepository, WorldRepository>();
 
+            services.AddTransient<GeoCoordsService>();
+
             services.AddTransient<WorldContextSeedData>();
 
             services.AddLogging();
 
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(config =>
+            config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +65,13 @@ namespace TheWorld
             IHostingEnvironment env, ILoggerFactory factory,
             WorldContextSeedData seeder )
         {
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<TripViewModel, Trip>().ReverseMap();
+                config.CreateMap<StopViewModel, Stop>().ReverseMap();
+
+            });
+
             if (env.IsEnvironment("Development"))
             {
                 app.UseDeveloperExceptionPage();
